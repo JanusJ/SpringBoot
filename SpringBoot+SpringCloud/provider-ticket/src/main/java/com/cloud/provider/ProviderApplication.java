a5cb0017c1ccd1ff6456108a51f6cdfc7e96adf0
Janus <1274654983@qq.com>
2018-07-19 18:12:10 +0800

    Update ProviderApplication.java

--- SpringBoot+SpringCloud/provider-ticket/src/main/java/com/cloud/provider/ProviderApplication.java
+++ SpringBoot+SpringCloud/provider-ticket/src/main/java/com/cloud/provider/ProviderApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ProviderApplication {
 
 	public static void main(String[] args) {
